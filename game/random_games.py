4fca267b9503710f189941412c9550451fbca639
kacper776 <kacper1342@gmail.com>
2022-09-07 22:39:28 +0200

    removed debug

--- game/random_games.py
+++ game/random_games.py
@@ -36,10 +36,7 @@ def random_game(game: SevenWonders, first_move: Move, nr: int,
                               if game.hand[player]]   
             while active_players:
                 for player in active_players:
-                    moves = game.moves(player) #######
-                    if not moves: #########
-                        print(player, game.hand[player])
-                    move = semi_random_move(moves)
+                    move = semi_random_move(game.moves(player))
                     if not first_move_done and player == nr:
                         move = first_move
                         first_move_done = True
